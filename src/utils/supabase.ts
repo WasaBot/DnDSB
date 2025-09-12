5ea2405a5dc8328711f32b181d21dace838b094a
Robin Kayser <robin-kayser@web.de>
2025-09-12 10:45:32 +0200

    fixing env

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if(supabaseUrl === undefined || supabaseKey === undefined) {
+    throw new Error("Missing Supabase environment variables");
+}
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
